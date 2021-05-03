e8146176369fdb843eaa3e8b9f97766cb5b9a3f4
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-05-03 10:05:02 -0400

    Add project files.

---
+++ RestaurantRaterMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRaterMvc.Startup))]
+namespace RestaurantRaterMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
